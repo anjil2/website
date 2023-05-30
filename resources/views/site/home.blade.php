4b708860d65f80abaf67ace1347e695724b56b90
Anjil Khatri <anjilkhatri980@gmail.com>
2023-05-30 11:29:21 +0545

    initial setup

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('site/css/style.css') }}">
+</head>
+
+<body>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quod molestias culpa maiores veniam? Consectetur quos, perspiciatis minus qui error odio sequi reprehenderit laborum quis impedit accusantium nemo nostrum voluptates!
+    </p>
+</body>
+</html>
